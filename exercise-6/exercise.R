d39d1a43b0394fbfedef3732f677aa55bbd1678b
Soobin Kwon <soobkwon@uw.edu>
2017-01-18 23:06:32 -0800

    Update Module 9 Exercise 6

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -3,22 +3,21 @@
 husky.games.2015 <- read.csv("data/huskys_2015.csv, stringsAsFactors=FALSE")
 
 # Create a vector of the teams that the Huskies played against during that season
-teams <- c("Boise State", "Sacramento", "Utah State", "California", "Southern Cal", "Oregon", "Stanford", "Arizona", "Utah", "Arizona State", "Oregon State", "Washington State", "Southern Mississippi")
+not.huskies <- husky.games.2015$opponent
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-husky.scores <- c(13, 49, 31, 24, 17, 20, 14, 49, 23, 23, 52, 45, 44)
+husky.scores <- husky.games.2015$uw_score
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-
-
+rushing.yards <- husky.games.2015$rushing_yards
+passing.yards <- husky.games.2015$passing_yards
 
 # Create a variable called `combined.yards` that is the total yardage of the Huskies for each game
-
-
+combined.yards <- passing.yards + rushing.yards
 
 # Add the `combined.yards` vector to the dataframe with the column name being `combined`
-
+husky.games.2015["combined"] <- combined.yards
 
 
 # Write the dataframe to a new `.csv` file called `huskies_2015_improved.csv` into the `data directory`
